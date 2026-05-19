f0280d75c1b037cb47b37737d3aa16922ec60a02
Ritika Soni <ritikasoni6759@gmail.com>
2026-05-19 23:33:20 +0530

    Solved new dsa problem

---
+++ array/hard/slidingwindowmax.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector <int> vec;
+        deque<int> dq;
+        for(int r=0;r<n;r++)
+        {
+            while(!dq.empty() && dq.front()<=r-k)
+            dq.pop_front();
+
+            while(!dq.empty() && nums[dq.back()]<nums[r])
+            dq.pop_back();
+
+            dq.push_back(r);
+
+            if(r>=k-1)
+            vec.push_back(nums[dq.front()]);
+        }
+        return vec;
+    }
+};
